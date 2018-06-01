37f51096597fe32656ea9fb45d4f21be4ff3dfdd
jmcgrathstuy <jmcgrath@stuy.edu>
2018-06-01 15:31:31 -0400

    Added getters and setters to Player class

--- Player.java
+++ Player.java
@@ -1,3 +1,7 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
 public class Player{
 
     private String name;
@@ -10,6 +14,27 @@ public class Player{
 	score = 0;
 	hand = new ArrayList<Piece>();
     }
+
+    //notes to self:
+    //don't need a fillHand() method, driver will do that ( i think)
+
+    public void setName( String n){
+	name = n;
+    }
+    public String getName(){
+	return name;
+    }
+    public void setScore( int s){
+	score = s;
+    }
+    public void addScore( int s){
+        score =+ s;
+    }
+    public int getScore(){
+	return score;
+    }
+
+    
 	
 
     
