2d099761cb8d765534ae8e131b544ed5f2de3f72
jmcgrathstuy <jmcgrath@stuy.edu>
2018-06-06 15:28:29 -0400

    Fixed Specialty(), added a version of specialty() that returns a string (DW, DL, TW, TL), and wrote the toString for Board

--- Board.java
+++ Board.java
@@ -34,9 +34,29 @@ public class Board{
     //toString
     public String toString(){
 	String str = "";
+	for( int i = 0; i < 30; i++){
+		str += "--";
+	    }
+	str += "\n";
 	for( int sR = 0; sR < 15; sR++){
+	    str+= "|";
 	    for( int sC = 0; sC < 15; sC++){
-		if( 
+		if( brd[sR][sC] != null){
+		    str += " " + brd[sR][sC].getLetter() + " |";
+		}
+		else{
+		    str += specialtyStr( sR, sC);
+		    str += " |";
+		}
+	    }
+	    str += "\n";
+	    for( int i = 0; i < 30; i++){
+		str += "--";
+	    }
+	    str += "\n";
+	}
+	return str;
+    }
 	
     
     //Sets brd as a new, empty 15x15 board
@@ -106,21 +126,121 @@ public class Board{
 	       || ( y == 9 && x == 9)
 	       || ( y == 11 && x == 3)
 	       || ( y == 11 && x == 11)
-	       || ( y == 14 && x == 7)
+	       || ( y == 14 && x == 6)
 	       || ( y == 14 && x == 8)
 		  ){
 	    return 3;
 	}
+	if(       ( y == 0 && x == 3)
+	       || ( y == 0 && x == 11)
+	       || ( y == 3 && x == 0)
+	       || ( y == 3 && x == 14)
+	       || ( y == 14 && x == 3)
+	       || ( y == 11 && x == 0)
+	       || ( y == 11 && x == 14)
+	       || ( y == 14 && x == 11)
+		  ){
+	    return 4;
+	}
 	else{
     
-	return 999;
+	return 0;
+	}
+    }
+     public String specialtyStr(int x, int y){
+	//series of ifs to see if it is one of the special spots
+	//OR add a list to this class of special coords and loop through that?
+	//either way writing this will be easy but time consuming
+	if(       ( x == 1 && y == 2)
+	       || ( x == 1 && y == 12)
+	       || ( x == 2 && y == 1)
+	       || ( x == 2 && y == 4)
+	       || ( x == 2 && y == 10)
+	       || ( x == 2 && y == 13)
+	       || ( x == 4 && y == 2)
+	       || ( x == 4 && y == 6)
+	       || ( x == 4 && y == 8)
+	       || ( x == 4 && y == 12)
+	       || ( x == 6 && y == 4)
+	       || ( x == 6 && y == 10)
+	       || ( x == 8 && y == 4)
+	       || ( x == 8 && y == 10)
+	       || ( x == 10 && y == 2)
+	       || ( x == 10 && y == 6)
+	       || ( x == 10 && y == 8)
+	       || ( x == 10 && y == 12)
+	       || ( x == 12 && y == 1)
+	       || ( x == 12 && y == 4)
+	       || ( x == 12 && y == 10)
+	       || ( x == 12 && y == 13)
+	       || ( x == 13 && y == 2)
+	       || ( x == 13 && y == 12) ){
+	    return "DL";
+	}
+	if(       ( x == 1 && y == 9)
+	       || ( x == 1 && y == 5)
+	       || ( x == 3 && y == 7)
+	       || ( x == 5 && y == 1)
+	       || ( x == 5 && y == 13)
+	       || ( x == 7 && y == 3)
+	       || ( x == 7 && y == 11)
+	       || ( x == 9 && y == 1)
+	       || ( x == 9 && y == 13)
+	       || ( x == 11 && y == 7)
+	       || ( x == 13 && y == 5)
+	       || ( x == 13 && y == 9)
+		  ){
+	    return "DW";
+	}
+	if(       ( y == 0 && x == 6)
+	       || ( y == 0 && x == 8)
+	       || ( y == 3 && x == 3)
+	       || ( y == 3 && x == 11)
+	       || ( y == 5 && x == 5)
+	       || ( y == 5 && x == 9)
+	       || ( y == 6 && x == 0)
+	       || ( y == 6 && x == 14)
+	       || ( y == 8 && x == 0)
+	       || ( y == 8 && x == 14)
+	       || ( y == 9 && x == 5)
+	       || ( y == 9 && x == 9)
+	       || ( y == 11 && x == 3)
+	       || ( y == 11 && x == 11)
+	       || ( y == 14 && x == 6)
+	       || ( y == 14 && x == 8)
+		  ){
+	    return "TL";
+	}
+	if(       ( y == 0 && x == 3)
+	       || ( y == 0 && x == 11)
+	       || ( y == 3 && x == 0)
+	       || ( y == 3 && x == 14)
+	       || ( y == 14 && x == 3)
+	       || ( y == 11 && x == 0)
+	       || ( y == 11 && x == 14)
+	       || ( y == 14 && x == 11)
+		  ){
+	    return "TW";
+	}
+	else{
+    
+	return "  ";
 	}
     }    
     
     
     
 
-    public static void main( String[] args){}
+    public static void main( String[] args){
+	Board b = new Board();
+	System.out.println( b.toString());
+	Piece p = new Piece( 'A', 7);
+	b.placePiece( p, 4, 2);
+        System.out.println( b.toString());
+	
+
+
+    }
 	
 
 
